06c0de85e3c5ca46b7b83006969fb546d9b7a02e
Naveen <Naveen18jan93@gmail.com>
2016-11-25 19:51:02 -0500

    added database go program

---
+++ database/database.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "database/sql"
+    "strconv"
+    "testing"
+    "fmt"
+    _ "github.com/go-sql-driver/mysql"
+)
+
+type Student struct{
+	Id int
+	Name string
+	Uni string
+	Age int
+}
+
+func main(){
+	br := testing.Benchmark(BenchmarkgetData)
+    fmt.Println(br)
+}
+
+func getData() string{
+	database := "Scala"
+	user := "guest"
+	password := "mysql123"
+	con, err := sql.Open("mysql", user+":"+password+"@/"+database)
+	if(err!=nil){
+		panic(err.Error())
+	}
+	query := "select * from student where name = ?"
+	row := con.QueryRow(query, "Shantanu")
+	rs := new(Student)
+	err = row.Scan(&rs.Id, &rs.Name, &rs.Uni, &rs.Age)
+	if(err!=nil){
+		fmt.Println(err);
+	}
+	result := "Student id is: " + strconv.Itoa(rs.Id) + "\n" + "Student name is: " + rs.Name + "\n" + "Student university is: " + rs.Uni + "\n" + "Student age is: " + strconv.Itoa(rs.Age)
+	defer con.Close()	
+	return result
+}
+
+func BenchmarkgetData(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+   		getData()
+	}
+}
