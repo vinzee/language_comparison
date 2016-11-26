c592fc274731c7bf6f1f1483b1ff3874817c40e7
Naveen <Naveen18jan93@gmail.com>
2016-11-25 19:54:53 -0500

    added comments

--- inmemory_cache/go/map.go
+++ inmemory_cache/go/map.go
@@ -12,11 +12,11 @@ var m = make(map[int]string)
 func Request(key int) (string, bool){
 	if val, ok := m[key];ok{
 		return val, true // hit
-	} else{
-		if(len(m)>1000){	 // miss
+	} else{ // miss
+		if(len(m)>1000){ // if size of m is >1000 need to evict
 			m[1] = "Testing val"
 			//fmt.Printf("evict\n")
-		} else {
+		} else {	// else append the new key value pair to the map
 			 l := len(m)
 			 m[l] = "Testing val\n"
 		}
@@ -26,7 +26,7 @@ func Request(key int) (string, bool){
 
 func BenchmarkRequest(b *testing.B) {
     for i := 0; i < b.N; i++ {
-    	 k := rand.Intn(3000)
+    	 k := rand.Intn(3000) // random integer between 0 to 3000
          _, _ = Request(k)
 	}
 }
