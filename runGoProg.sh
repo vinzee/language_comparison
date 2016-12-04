cac5b1f429c1a6389229fca6bde6f87432b02cfd
Naveen <Naveen18jan93@gmail.com>
2016-12-03 19:22:35 -0500

    added shell script to execute all go programs

---
+++ runGoProg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+printf "\n"
+go run network_programming/go/server.go	&
+sleep 2
+go run network_programming/go/client.go	
+printf "\n"
+go run concurrency/go/parallel_fibo.go
+printf "\n"
+go run database/go/database.go
+printf "\n"
+go run inmemory_cache/go/map.go
+printf "\n"
+go run json_parser/go/jsonParsing.go
+printf "\n"
+go run pattern_matching/go/pattern_matching.go	
+# etc.
