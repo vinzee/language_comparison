6a1346804d2e6de8e774bea0b3e0c4cf2f1da465
Shantanu Hirlekar <you@example.com>
2016-12-04 16:14:48 -0500

    Added benchmarking for the scala DB program.

--- database/Scala/ScalaDatabase.scala
+++ database/Scala/ScalaDatabase.scala
@@ -29,10 +29,26 @@ object ScalaDatabase{
 		
 	}
 
+	def time[R](block: => R): R = {  
+    	val t0 = System.nanoTime()
+    	val result = block    // call-by-name
+    	val t1 = System.nanoTime()
+    	println("Elapsed time: " + (t1 - t0) + "ns")
+    	result
+}
+
 	def main(args: Array[String]): Unit = {
 	  
-		val result = getData()
+		val result = time {getData()}
 		println(result)
+
+		val mb = 1024*1024
+        val runtime = Runtime.getRuntime
+        println("\nMemory in MB")
+        println("** Used Memory:  " + (runtime.totalMemory - runtime.freeMemory) / mb)
+        println("** Free Memory:  " + runtime.freeMemory / mb)
+        println("** Total Memory: " + runtime.totalMemory / mb)
+        println("** Max Memory:   " + runtime.maxMemory / mb)
 
 	}
 }
