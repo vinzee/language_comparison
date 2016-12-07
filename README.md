57eebc9089fd1dc54b774aa6233e556713c2c4be
Vineet Ahirkar <vinzee93@gmail.com>
2016-12-07 00:43:49 -0500

    database operations

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ $ mix run hello_world.exs
 - Installing Elixir - https://bordeltabernacle.github.io/2016/01/06/notes-on-elixir-upgrading-toinstalling-v12-on-ubuntu.html
 - http://elixir-lang.org/docs/stable/elixir/api-reference.html
 - https://www.tutorialspoint.com/elixir
+- https://elixir.libhunt.com
+- https://github.com/drobakowski/awesome-erlang
 - Elixir basic Syntax Overview - http://elixir-lang.org/crash-course.html
 - Benchfella Elixir profiler - https://github.com/alco/benchfella
 - JSON parser - https://github.com/cblage/elixir-json
--- database/elixir/database_operations.exs
+++ database/elixir/database_operations.exs
@@ -2,22 +2,20 @@ defmodule DatabaseOperations do
 	use Benchfella
 	Benchfella.start
 
-	def get_student(name) do
-		# make connection to mysql
-		{_, db} = Mariaex.Connection.start_link(username: "root", password: 'admin123', database: "elixir")
+	# make connection to mysql
+	@db Mariaex.Connection.start_link(username: "root", password: 'admin123', database: "elixir")
 
+	def get_student(db, name) do
 		# fire query
 		{_, data} = Mariaex.query(db, "SELECT * FROM student where name='#{name}'")
 
 		rows = Map.get(data, :rows)
-
-		# TODO - find a way to close the connection
 		# IO.inspect rows
-
 		rows
 	end
 
 	bench "DatabaseOperations" do
-		get_student('vineet')
+		{_, db} = @db
+		get_student(db, 'vineet')
 	end
 end
